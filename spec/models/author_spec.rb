3c8a84c4e6ee983ce34a0bbdacc093981d8fd006
SinNeax <Nick.Bessin@student.hpi.uni-potsdam.de>
2021-11-18 13:39:51 +0100

    Failing test for #4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "Author model", type: :model do
+  it "should test the full model" do
+    author = Author.new("Alan", "Turing")
+    expect(author.first_name).to eq("Alan")
+    expect(author.last_name).to eq("Turing")
+    expect(author.homepage).to eq("http://wikipedia.de/Alan_Turing")
+    expect(author.name).to eq("Alan Turing")
+  end
 end
