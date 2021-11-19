c05077cce369848caf9d6c0e64b00fee0aff91f1
SinNeax <Nick.Bessin@student.hpi.uni-potsdam.de>
2021-11-19 15:25:50 +0100

    Failing test for #8

--- spec/features/author/overview_page_spec.rb
+++ spec/features/author/overview_page_spec.rb
@@ -5,4 +5,11 @@ describe "Overview page", type: :feature do
     visit authors_path
     expect(page).to have_link 'New', href: new_author_path
   end
+
+  it "should have a link to delete an author" do
+    @author = Author.create(first_name: 'Alan', last_name: 'Turing')
+    puts @author.name
+    visit authors_path
+    expect(page).to have_link 'Delete'
+  end
 end
