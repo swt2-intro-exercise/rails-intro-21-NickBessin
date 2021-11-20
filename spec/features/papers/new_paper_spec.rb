bb4b3ccd8ea039c58b93352318d8509394965303
SinNeax <Nick.Bessin@student.hpi.uni-potsdam.de>
2021-11-20 09:48:30 +0100

    fixes #10

--- spec/features/papers/new_paper_spec.rb
+++ spec/features/papers/new_paper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "New paper page", type: :feature do
   it "should not validate w/o title" do
-    paper = Paper.new(title: "Covid 19-Pandemic", venue: "RKI", year: "2020")
+    paper = Paper.new(title: nil, venue: "RKI", year: "2020")
     expect(paper).to_not be_valid
   end
 end
