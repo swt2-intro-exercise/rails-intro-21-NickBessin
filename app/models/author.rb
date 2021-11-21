d9cf8c4fbb76a02bbdecee97011e001feaa70989
SinNeax <Nick.Bessin@student.hpi.uni-potsdam.de>
2021-11-21 14:22:04 +0100

    fixes #14

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ApplicationRecord
-	# has_and_belongs_to_many :papers
+	has_and_belongs_to_many :papers
     validates :last_name, presence: true
     
     def new(first_name, last_name, homepage)
