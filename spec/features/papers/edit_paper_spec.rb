9153c8e142439c3f88f916c3ad1e8b403d276c56
SinNeax <Nick.Bessin@student.hpi.uni-potsdam.de>
2021-11-22 15:15:48 +0100

    Failing test for #16

---
+++ spec/features/papers/edit_paper_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Edit page", type: :feature do
+  it "should allow to select paper authors from a multiple select box" do
+	visit new_author_path
+    fill_in 'author[first_name]', with: "Alan"
+    fill_in 'author[last_name]', with: "Turing"
+    submit_form
+
+	visit new_paper_path
+    expect(page.find(:select, 'paper[author_ids][]')[:multiple]).to be
+  end
+end
