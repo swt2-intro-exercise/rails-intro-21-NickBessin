044bce4776cf709fccc2df419fdd99a1aeee2a97
Christoph Matthies <chrisma@users.noreply.github.com>
2021-11-25 23:38:06 +0100

    Add paper should save changes to the author list test

--- spec/features/authors_papers/many_to_many_spec.rb
+++ spec/features/authors_papers/many_to_many_spec.rb
@@ -6,4 +6,24 @@ describe "Paper model", type: :feature do
 		expect(@paper.authors.empty?).to be
     end
 
+    it "should save changes to the author list" do
+        @alan = FactoryBot.create(:author)
+        # Given a paper with one author
+        @paper = FactoryBot.create(:paper, authors: [@alan])
+        # And another author called 'Peter Plagiarist'
+        @peter = FactoryBot.create(:author, first_name: 'Peter', last_name: 'Plagiarist', homepage: '')
+        # When a user visits the paper's edit page
+        visit edit_paper_path(@paper)
+        # And then selects 'Peter Plagiarist' as author
+        find(:css, 'select').find(:css, "option[value='#{@peter.id}']").select_option
+        # And submits the form
+        find(:css, 'input[type="submit"]').click
+
+        # Reload object from database as it has been modified
+        # by submitting the form
+        @paper.reload
+        # Then 'Peter Plagiarist' and 'Alan turing' should be authors of the paper
+        expect(@paper.authors.count).to eq(2)
+        expect(@paper.authors).to contain_exactly(@alan, @peter)
+    end
 end
